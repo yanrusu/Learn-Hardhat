c45ef85f279882b96851d579992266fda744f326
yanrusu <yanrusu0701@gmail.com>
2025-11-28 12:56:14 +0800

    commit

--- contracts/checkaddr.sol
+++ contracts/checkaddr.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 contract CheckAddr {
     function isContract(address addr) public view returns (string memory) {
         if (addr.code.length == 0) {
-            return "EOA"; // Address zero is not a contract
+            return "EOA"; 
         }
         return "Contract";
     }
